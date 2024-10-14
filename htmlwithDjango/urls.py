a57de449048bf1982f6b460859cd8737be0db5b2
Madhava Bhumireddy <Madhava.Bhumireddy@connxai.com>
2024-10-14 17:00:56 +0530

    urls changes

--- htmlwithDjango/urls.py
+++ htmlwithDjango/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home,name='home'),
     path('register/',views.register,name='register'),
-    path('login/',views.login,name='login')
+    path('login/',views.login,name='login'),
+path('login1/',views.login,name='login')
 ]
